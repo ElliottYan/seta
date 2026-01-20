03fa09e534b74610a61f664015cc6c4bf0867183
Jianhao Yan <elliottyan37@gmail.com>
2026-01-20 13:57:40 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# 1. install miniforge
+SCRIPT_PATH=$(realpath "$0")
+PROJECT_DIR=$(dirname "$SCRIPT_PATH")
+
+cd ${PROJECT_DIR}/external/camel && pip3.12 install -e .
+cd ${PROJECT_DIR}/external/terminal-bench && pip3.12 install -e .
+cd ${PROJECT_DIR}/external/AReaL && pip3.12 install -e .[all]

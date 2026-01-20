507ec8c59c3e8bf23f99cf9318be894d20ab0fe6
Jianhao Yan <elliottyan37@gmail.com>
2026-01-20 21:50:05 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,5 +4,5 @@ SCRIPT_PATH=$(realpath "$0")
 PROJECT_DIR=$(dirname "$SCRIPT_PATH")
 
 cd ${PROJECT_DIR}/external/camel && pip3.12 install -i https://mirrors.aliyun.com/pypi/simple/ --resume-retries 5 -e .
-cd ${PROJECT_DIR}/external/terminal-bench && pip3.12 install -i https://mirrors.aliyun.com/pypi/simple/ --resume-retries 5 -e .
+# cd ${PROJECT_DIR}/external/terminal-bench && pip3.12 install -i https://mirrors.aliyun.com/pypi/simple/ --resume-retries 5 -e .
 # cd ${PROJECT_DIR}/external/AReaL && pip3.12 install -i https://mirrors.aliyun.com/pypi/simple/ --resume-retries 5 -e .[all]
